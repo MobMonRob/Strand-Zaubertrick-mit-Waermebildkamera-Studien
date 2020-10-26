2ad1504c43097583ceec7b8cd8c4ed153325023b
MM53 <28218664+MM53@users.noreply.github.com>
2020-10-26 17:44:09 +0100

    Cleanup MainActivity

--- app/src/main/java/de/dhbw/research/human/fade/out/MainActivity.java
+++ app/src/main/java/de/dhbw/research/human/fade/out/MainActivity.java
@@ -1,109 +1,110 @@
 package de.dhbw.research.human.fade.out;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import com.flir.flironesdk.Device;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import de.dhbw.research.human.fade.out.flir.FlirDevice;
+
+public class MainActivity extends AppCompatActivity{
+
+    private String[] neededPermissions = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+    private FlirDevice flirDevice;
+
+    private FloatingActionButton startButton;
+    private FloatingActionButton stopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        createFlirDevice();
+
+        startButton = findViewById(R.id.start);
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Device.startDiscovery(getApplicationContext(), new Device.Delegate() {
-                    @Override
-                    public void onTuningStateChanged(Device.TuningState tuningState) {
-
-                    }
-
-                    @Override
-                    public void onAutomaticTuningChanged(boolean b) {
-
-                    }
-
-                    @Override
-                    public void onDeviceConnected(Device device) {
-                        System.out.println("Device connected");
-                    }
-
-                    @Override
-                    public void onDeviceDisconnected(Device device) {
-                        System.out.println("Device disconnected");
-                    }
-                });
+            public void onClick(View v) {
+                onStartButtonClicked();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        stopButton = findViewById(R.id.stop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStopButtonClicked();
+            }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        while (!permissionsGranted()) {
+            requestPermissions();
         }
 
-        return super.onOptionsItemSelected(item);
+        enableStoppedState();
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Device.startDiscovery(getApplicationContext(), new Device.Delegate() {
-            @Override
-            public void onTuningStateChanged(Device.TuningState tuningState) {
+    protected void onDestroy() {
+        flirDevice.stop();
+        flirDevice = null;
 
-            }
-
-            @Override
-            public void onAutomaticTuningChanged(boolean b) {
-
-            }
-
-            @Override
-            public void onDeviceConnected(Device device) {
-                System.out.println("Device connected");
-            }
-
-            @Override
-            public void onDeviceDisconnected(Device device) {
-                System.out.println("Device disconnected");
-            }
-        });
+        super.onDestroy();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Device.stopDiscovery();
+    private void onStartButtonClicked() {
+        if (flirDevice == null) {
+            createFlirDevice();
+        }
+
+        flirDevice.start();
+        enableStartedState();
+    }
+
+    private void createFlirDevice() {
+        flirDevice = new FlirDevice(this);
+    }
+
+    private void onStopButtonClicked() {
+        flirDevice.stop();
+        enableStoppedState();
+    }
+
+    private void enableStartedState() {
+        startButton.setEnabled(false);
+        stopButton.setEnabled(true);
+    }
+
+    private void enableStoppedState() {
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+    }
+
+    private boolean permissionsGranted() {
+        boolean permissionsGranted = true;
+
+        for (String permission : neededPermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsGranted = false;
+                break;
+            }
+        }
+
+        return permissionsGranted;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, neededPermissions, 0);
     }
 }
