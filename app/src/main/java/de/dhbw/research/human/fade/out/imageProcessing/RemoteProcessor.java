dfd1604b1801321fed375c60faf7e2585be2599b
MM53 <28218664+MM53@users.noreply.github.com>
2020-11-26 16:00:08 +0100

    Send compressed image directly

--- app/src/main/java/de/dhbw/research/human/fade/out/imageProcessing/RemoteProcessor.java
+++ app/src/main/java/de/dhbw/research/human/fade/out/imageProcessing/RemoteProcessor.java
@@ -5,19 +5,16 @@ import android.widget.ImageView;
 
 import com.flir.flironesdk.RenderedImage;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
 import de.dhbw.research.human.fade.out.remote.AsyncClient;
-import de.dhbw.research.human.fade.out.remote.SimpleClient;
 import de.dhbw.research.human.fade.out.remote.dto.ThermalImage;
 
 public class RemoteProcessor implements ImageProcessor {
 
     private AsyncClient client;
     private ImageView imageView;
-    private int[] lastVisualPixels;
+    private Bitmap lastVisualImage;
+    private ThermalImage thermalImage;
+
 
     public RemoteProcessor(final ImageView imageView) {
         this.imageView = imageView;
@@ -33,30 +30,10 @@ public class RemoteProcessor implements ImageProcessor {
     @Override
     public void processImage(final RenderedImage image) {
         if (image.imageType() == RenderedImage.ImageType.VisibleAlignedRGBA8888Image) {
-            Bitmap bitmap = image.getBitmap();
-            lastVisualPixels = new int[bitmap.getWidth() * bitmap.getHeight()];
-            bitmap.getPixels(lastVisualPixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+            lastVisualImage = image.getBitmap();
         } else if (image.imageType() == RenderedImage.ImageType.ThermalRadiometricKelvinImage) {
-            ThermalImage thermalImage = new ThermalImage(image.width(), image.height(), image.thermalPixelValues(), lastVisualPixels);
-//            try {
-//                sizeof(thermalImage);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+            thermalImage = new ThermalImage(image.width(), image.height(), image.thermalPixelValues(), lastVisualImage);
             client.sendImage(thermalImage, true);
         }
     }
-
-    private int sizeof(Object obj) throws IOException {
-
-        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream);
-
-        objectOutputStream.writeObject(obj);
-        objectOutputStream.flush();
-        objectOutputStream.close();
-
-        return byteOutputStream.toByteArray().length;
-    }
 }
-
