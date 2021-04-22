7ca21ec1eb6cc55970cbf8f169bd9db900425459
MM53 <28218664+mm53@users.noreply.github.com>
2021-04-22 22:47:42 +0200

    Add setup.py

---
+++ python-server/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-remote-processor',
+    version='0.1',
+    description='',
+    package_dir={'': 'lib'},
+    packages=['python-remote-processor']
+)
