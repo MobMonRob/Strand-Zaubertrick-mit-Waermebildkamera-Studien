e357d9eb36c05084c9cf14e859960e17b31dfebc
MM53 <28218664+mm53@users.noreply.github.com>
2021-05-13 15:19:35 +0200

    Load opencv for capturing videos

--- server/src/main/java/de/dhbw/research/human/fade/out/remote/server/CopyServer.java
+++ server/src/main/java/de/dhbw/research/human/fade/out/remote/server/CopyServer.java
@@ -2,12 +2,17 @@ package de.dhbw.research.human.fade.out.remote.server;
 
 import de.dhbw.research.human.fade.out.remote.imageProcessor.CaptureImageProcessor;
 import de.dhbw.research.human.fade.out.remote.imageProcessor.CopyImageProcessor;
+import nu.pattern.OpenCV;
+import org.opencv.core.Core;
 
 import java.io.FileNotFoundException;
 
 public class CopyServer {
 
     public static void main(String[] args) throws FileNotFoundException {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        OpenCV.loadShared();
+
         if (args.length == 2 && args[0].equals("record")) {
             new Server(4444, new CopyImageProcessor(), new CaptureImageProcessor(args[1])).start();
         } else {
